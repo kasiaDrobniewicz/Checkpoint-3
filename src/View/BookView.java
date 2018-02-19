1473bc7bd26da8cce9a4dc96addcbc103c72f24a
Kasia Drobniewicz <kasia.drobniewicz@gmail.com>
2018-02-19 12:22:16 +0100

    Add BookView file

---
+++ src/View/BookView.java
@@ -0,0 +1,44 @@
+package View;
+
+import BookModel.BookModel;
+
+import java.util.Scanner;
+
+public class BookView {
+
+    public static void displayText(String text) {
+        System.out.println(text);
+    }
+
+    public static BookModel askForBookData() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please type the new ISBN number: ");
+        int ISBN = scanner.nextInt();
+
+        System.out.println("Please type the autor: ");
+        String autor = scanner.next();
+
+        System.out.println("Please type the title: ");
+        String title = scanner.next();
+
+        System.out.println("Please type the publisher: ");
+        String publisher = scanner.next();
+
+        System.out.println("Please type the publisher year: ");
+        int publisherYear = scanner.nextInt();
+
+        System.out.println("Please type the publisher price: ");
+        int price = scanner.nextInt();
+
+        System.out.println("Please type the type: ");
+        String type = scanner.next();
+
+
+        BookModel book= new BookModel(ISBN,autor,title,publisher,publisherYear,price,type); {
+        }
+
+        return book;
+    }
+
+}
