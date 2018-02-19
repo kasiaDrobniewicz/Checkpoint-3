98308fe13265d7140e2270285bde4806d9ae1b0f
Kasia Drobniewicz <kasia.drobniewicz@gmail.com>
2018-02-19 10:35:58 +0100

    Fix constructor and data type of type atribute

--- src/BookModel/EBook.java
+++ src/BookModel/EBook.java
@@ -4,9 +4,12 @@ public class EBook extends BookModel {
 
     private String type;
 
-    public EBook(int ISBN, String autor, String title, String publisher, int publisherYear, int price) {
-        super(ISBN, autor, title, publisher, publisherYear, price);
+    public EBook(String type) {
         this.type = type;
+    }
 
+    public EBook(int ISBN, String autor, String title, String publisher, int publisherYear, int price, String type, String type1) {
+        super(ISBN, autor, title, publisher, publisherYear, price, type);
+        this.type = type1;
     }
 }
