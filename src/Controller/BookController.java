0d2a306c482ac55955d1b246faa0c1543296731f
Kasia Drobniewicz <kasia.drobniewicz@gmail.com>
2018-02-19 13:12:22 +0100

    Add addBook, deleteBook, updateBook methods

--- src/Controller/BookController.java
+++ src/Controller/BookController.java
@@ -3,6 +3,8 @@ import BookDao.BookDao;
 import BookModel.BookModel;
 import View.BookView;
 
+import java.util.List;
+
 public class BookController {
 
     private BookDao bookDao;
@@ -17,4 +19,36 @@ public class BookController {
         BookModel book = bookView.askForBookData();
         bookDao.addBook(book);
     }
+
+    public void updateBook() {
+        List<BookModel> books = bookDao.getAllBooks();
+        for(int i = 0; i < books.size(); i++) {
+            BookView.displayBookData(books.get(i));
+        }
+
+        int ISBN = BookView.getBookISBN();
+        BookModel book = BookView.askForBookData();
+        book.setISBN(ISBN);
+        bookDao.updateBook(book);
+    }
+
+
+
+    public void getBookByTitle(String title) {
+        List<BookModel> books = bookDao.getBookByTitle(title);
+
+        for(int i = 0; i < books.size(); i++) {
+            BookView.displayBookData(books.get(i));
+
+        }
+    }
+    public void deleteBookByTitle(String title) {
+        List<BookModel> books = bookDao.getBookByTitle(title);
+
+        for(int i = 0; i < books.size(); i++) {
+            bookDao.deleteBook(books.get(i));
+            System.out.println(books.get(i));
+        }
+    }
+
 }
